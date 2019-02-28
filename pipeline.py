8dd2147d77ce1bed54a8e10d108c3da06631f6dc
admin <199524943@qq.com>
2019-02-28 15:54:50 +0800

    尝试过滤deepracer赛道。

--- pipeline.py
+++ pipeline.py
@@ -14,42 +14,137 @@ from moviepy.editor import VideoFileClip
 import line
 
 def thresholding(img):
+    #print(img.shape)
     #setting all sorts of thresholds
+    #cv2.imshow("orig", img)
     x_thresh = utils.abs_sobel_thresh(img, orient='x', thresh_min=10 ,thresh_max=230)
+    #cv2.imshow("x_thresh",x_thresh*255)
     mag_thresh = utils.mag_thresh(img, sobel_kernel=3, mag_thresh=(30, 150))
+    #cv2.imshow("mag_thresh",mag_thresh*255)
     dir_thresh = utils.dir_threshold(img, sobel_kernel=3, thresh=(0.7, 1.3))
+    #cv2.imshow("dir_thresh",dir_thresh*255)
     hls_thresh = utils.hls_select(img, thresh=(180, 255))
+    #cv2.imshow("hls_thresh",hls_thresh*255)
     lab_thresh = utils.lab_select(img, thresh=(155, 200))
+    #cv2.imshow("lab_thresh",lab_thresh*255)
     luv_thresh = utils.luv_select(img, thresh=(225, 255))
+    #cv2.imshow("luv_thresh",luv_thresh)
 
     #Thresholding combination
     threshholded = np.zeros_like(x_thresh)
     threshholded[((x_thresh == 1) & (mag_thresh == 1)) | ((dir_thresh == 1) & (hls_thresh == 1)) | (lab_thresh == 1) | (luv_thresh == 1)] = 1
-
+    #cv2.imshow("threshholded", threshholded*255)
+    #cv2.waitKey(0)
     return threshholded
 
 
-def processing(img,object_points,img_points,M,Minv,left_line,right_line):
-    #camera calibration, image distortion correction
-    undist = utils.cal_undistort(img,object_points,img_points)
-    #get the thresholded binary image
-    thresholded = thresholding(undist)
+
+def colorMask(name,img,hsv,lower,upper):
+
+
+    # 创建掩膜
+    mask = cv2.inRange(hsv, lower, upper)
+    # 将原图像和掩膜做位与运算
+    res = cv2.bitwise_and(img, img, mask=mask)
+
+    #cv2.imshow(name, res)
+    return res
+
+
+
+def road_line(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # 设置阈值下限和上限，去除背景颜色
+    lower_red = np.array([80, 0, 0])
+    upper_red = np.array([160, 255, 150])
+
+    lower_white = np.array([0, 0, 221])
+    upper_white = np.array([180, 30, 255])
+
+    lower_yellow = np.array([100, 43, 46])
+    upper_yellow = np.array([120, 255, 255])
+    # 创建掩膜
+    # 将原图像和掩膜做位与运算
+
+    white_img = colorMask("white_mask", img, hsv, lower_white, upper_white)
+    yellow_img = colorMask("yellow_mask", img, hsv, lower_yellow, upper_yellow)
+
+    img = cv2.bitwise_or(white_img, yellow_img)
+
+    #cv2.imshow("res", img)
+    return img
+
+def processing(img, object_points, img_points, M, Minv, left_line, right_line):
+    # camera calibration, image distortion correction
+    # undist = utils.cal_undistort(img,object_points,img_points)
+    cv2.imshow("img", img)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+    # 设置阈值下限和上限，去除背景颜色
+    lower_red = np.array([80, 0, 0])
+    upper_red = np.array([160, 255, 150])
+
+
+    lower_white = np.array([0, 0, 221])
+    upper_white = np.array([180, 30, 255])
+
+
+    lower_yellow = np.array([100,43,46])
+    upper_yellow = np.array([120,255,255])
+
+    lower_lane = np.array([0,0,50])
+    upper_lane = np.array([180,43,120])
+    # 创建掩膜
+    # 将原图像和掩膜做位与运算
+
+    white_img = colorMask("white_mask",img,hsv,lower_white,upper_white)
+    # yellow_img = colorMask("yellow_mask", img, hsv, lower_yellow, upper_yellow)
+    # lane_img = colorMask("lane_mask", img, hsv, lower_lane, upper_lane)
+
+
+    undist = img
+    gray = cv2.cvtColor(white_img,cv2.COLOR_RGB2GRAY)
+
+    ret, binary = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE)
+    ret, binary2 = cv2.threshold(gray, 200, 1, cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE)
+    #cv2.imshow("binary", binary)
+    rgb =cv2.cvtColor(binary, cv2.COLOR_GRAY2RGB)
+    cv2.imshow("rgb", rgb)
+
+
+    #cv2.imshow("binary2", binary)
+    # get the thresholded binary image
+    thresholded = thresholding(rgb)
+    cv2.imshow("thresholded*255", thresholded * 255)
     #perform perspective  transform
-    thresholded_wraped = cv2.warpPerspective(thresholded, M, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+    thresholded_wraped = cv2.warpPerspective(binary2, M, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+
+    cv2.imshow("thresholded_wraped*255", thresholded_wraped*255)
+
 
     #perform detection
     if left_line.detected and right_line.detected:
+        print("find line")
         left_fit, right_fit, left_lane_inds, right_lane_inds = utils.find_line_by_previous(thresholded_wraped,left_line.current_fit,right_line.current_fit)
     else:
+        print("no find line")
         left_fit, right_fit, left_lane_inds, right_lane_inds = utils.find_line(thresholded_wraped)
     left_line.update(left_fit)
     right_line.update(right_fit)
 
     #draw the detected laneline and the information
     area_img = utils.draw_area(undist,thresholded_wraped,Minv,left_fit, right_fit)
-    curvature,pos_from_center = utils.calculate_curv_and_pos(thresholded_wraped,left_fit, right_fit)
-    result = utils.draw_values(area_img,curvature,pos_from_center)
+    cv2.imshow("area_img", area_img)
+    cv2.waitKey(50)
 
+    #print(area_img)
+    #curvature,pos_from_center = utils.calculate_curv_and_pos(thresholded_wraped,left_fit, right_fit)
+    #result = utils.draw_values(area_img,curvature,pos_from_center)
+    #result=area_img
+    result = rgb
     return result
 #
 #
@@ -59,9 +154,10 @@ cal_imgs = utils.get_images_by_dir('camera_cal')
 object_points,img_points = utils.calibrate(cal_imgs,grid=(9,6))
 M,Minv = utils.get_M_Minv()
 
-project_outpath = 'vedio_out/project_video_out.mp4'
-project_video_clip = VideoFileClip("project_video.mp4")
+project_outpath = 'vedio_out/test.mp4'
+project_video_clip = VideoFileClip("test.avi")
 project_video_out_clip = project_video_clip.fl_image(lambda clip: processing(clip,object_points,img_points,M,Minv,left_line,right_line))
+
 project_video_out_clip.write_videofile(project_outpath, audio=False)
 
 
--- utils.py
+++ utils.py
@@ -10,7 +10,8 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+np.set_printoptions(threshold=1e6)
+np.set_printoptions(linewidth= 1000)
 #get all image in the given directory persume that this directory only contain image files
 def get_images_by_dir(dirname):
     img_names = os.listdir(dirname)
@@ -32,13 +33,22 @@ def calibrate(images,grid=(9,6)):
             img_points.append(corners)
     return object_points,img_points
 
+# def get_M_Minv():
+#     src = np.float32([[(203, 720), (585, 460), (695, 460), (1127, 720)]])
+#     dst = np.float32([[(320, 720), (320, 0), (960, 0), (960, 720)]])
+#     M = cv2.getPerspectiveTransform(src, dst)
+#     Minv = cv2.getPerspectiveTransform(dst,src)
+#     return M,Minv
+
 def get_M_Minv():
-    src = np.float32([[(203, 720), (585, 460), (695, 460), (1127, 720)]])
-    dst = np.float32([[(320, 720), (320, 0), (960, 0), (960, 720)]])
+    #src = np.float32([[(0, 108), (49, 80), (160-49, 80), (160, 108)]])
+    src = np.float32([[(-160, 120), (0, 80), (160, 80), (320, 120)]])
+    dst = np.float32([[(0, 120), (0, 0), (160, 0), (160, 120)]])
     M = cv2.getPerspectiveTransform(src, dst)
     Minv = cv2.getPerspectiveTransform(dst,src)
     return M,Minv
-    
+
+
 #function takes an image, object points, and image points
 # performs the camera calibration, image distortion correction and 
 # returns the undistorted image
@@ -59,8 +69,10 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
         abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
     # Rescale back to 8 bit integer
     scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+
     # Create a copy and apply the threshold
     binary_output = np.zeros_like(scaled_sobel)
+
     # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
     binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
 
@@ -127,6 +139,7 @@ def lab_select(img, thresh=(0, 255)):
     return binary_output
 
 def find_line(binary_warped):
+
     # Take a histogram of the bottom half of the image
     histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
     # Find the peak of the left and right halves of the histogram
@@ -147,9 +160,13 @@ def find_line(binary_warped):
     leftx_current = leftx_base
     rightx_current = rightx_base
     # Set the width of the windows +/- margin
-    margin = 100
+   # print(f"window_height:{window_height}")
+    #margin = 100
+    np.int(binary_warped.shape[0] / nwindows)
+    margin = 20
     # Set minimum number of pixels found to recenter window
-    minpix = 50
+    minpix= 10
+    #minpix = np.int(binary_warped.shape[1]/nwindows)
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
     right_lane_inds = []
@@ -186,18 +203,22 @@ def find_line(binary_warped):
     lefty = nonzeroy[left_lane_inds] 
     rightx = nonzerox[right_lane_inds]
     righty = nonzeroy[right_lane_inds] 
-    
     # Fit a second order polynomial to each
-    left_fit = np.polyfit(lefty, leftx, 2)
-    right_fit = np.polyfit(righty, rightx, 2)
-    
+    try:
+
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+    except:
+        left_fit =[0,0,0]
+        right_fit=[0,0,0]
+
     return left_fit, right_fit, left_lane_inds, right_lane_inds
 
 def find_line_by_previous(binary_warped,left_fit,right_fit):
     nonzero = binary_warped.nonzero()
     nonzeroy = np.array(nonzero[0])
     nonzerox = np.array(nonzero[1])
-    margin = 100
+    margin = 13
     left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
     left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
     left_fit[1]*nonzeroy + left_fit[2] + margin))) 
@@ -211,9 +232,14 @@ def find_line_by_previous(binary_warped,left_fit,right_fit):
     lefty = nonzeroy[left_lane_inds] 
     rightx = nonzerox[right_lane_inds]
     righty = nonzeroy[right_lane_inds]
-    # Fit a second order polynomial to each
-    left_fit = np.polyfit(lefty, leftx, 2)
-    right_fit = np.polyfit(righty, rightx, 2)
+
+    try:
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+    except:
+        left_fit =[0,0,0]
+        right_fit=[0,0,0]
+
     return left_fit, right_fit, left_lane_inds, right_lane_inds
 
 def draw_area(undist,binary_warped,Minv,left_fit, right_fit):
